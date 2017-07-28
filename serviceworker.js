05f92640060f1074609bd74c89e6a0c557ee817c
Yanbin Ma <yianbin@qq.com>
2017-07-28 16:36:04 +0800

    feat: 使用 Service Worker 实现离线访问

---
+++ serviceworker.js
@@ -0,0 +1,86 @@
+const CACHE_VERSION = '0.0.1';
+const CACHE_FILES = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/link.html',
+  '/offline.html',
+  '/public/css/styles.css',
+  '/public/js/drawer.js',
+  '/public/img/cat.jpg',
+  '/public/img/redflag.jpg',
+  '/public/img/coconut.jpg'
+];
+const IGNORE_LISTS = [
+  'www.google-analytics.com',
+  'dn-lbstatics.qbox.me'
+];
+
+
+this.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then(function (cache) {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+this.addEventListener('fetch', function (event) {
+  // 在离线状态下跳过对统计代码的 Fetch
+  if (shouldAlwaysFetch(event.request)) {
+    event.respondWith(
+      fetch(event.request).then(function (response) {
+        return response;
+      })
+      .catch(function () {
+        return new Response('');
+      })
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          if (response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          let responseClone = response.clone();
+          caches.open(CACHE_VERSION).then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          return offlineResponse(event.request);
+        });
+      }
+    })
+  );
+});
+
+/**
+ * 用于清空旧的 Cache
+ */
+this.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (CACHE_VERSION !== key) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+let shouldAlwaysFetch = function (request) {
+  return request.method !== 'GET' || IGNORE_LISTS.some(function (domain) {
+    return request.url.match(domain);
+  })
+};
+
+let offlineResponse = function (request) {
+  return caches.match('/offline.html');
+};
